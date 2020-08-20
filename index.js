5b6b81c5622a48aaf914a27acfe46bfcf6073754
Steven Sussman <sjsussman@gmail.com>
2020-08-19 22:05:49 -0400

    completes tasks 1 and 2

--- index.js
+++ index.js
@@ -41,7 +41,25 @@ class Airplane {
 */
 
 class Person {
+  constructor (name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
 
+  eat(someFood){
+    if(this.stomach.length < 10){
+    this.stomach.push(someFood)
+    }
+  }
+
+  poop(){
+    this.stomach = [];
+  }
+
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
@@ -59,7 +77,26 @@ class Person {
 */
 
 class Car {
+constructor (model, milesPerGallon){
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this. odometer = 0;
+}
+fill(gallons){
+  this.tank += gallons;
+}
 
+drive(distance){
+  this.odometer += distance;
+  this.tank -= distance / this.milesPerGallon;
+  
+if (distance / this.milesPerGallon > this.tank){
+  this.odometer += this.tank*this.milesPerGallon;
+  this.tank = 0;
+  return `I ran out of fuel at ${this.odometer}`;
+}
+}
 }
 
 /*
